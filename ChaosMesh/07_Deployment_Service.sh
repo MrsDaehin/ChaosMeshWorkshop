d6f3acefd1efca7defcb6be39757291358dcf3f6
mrsdaehin <almudena.vivanco@gmail.com>
2023-04-28 04:16:53 -0400

    added more stuff

--- ChaosMesh/07_Deployment_Service.sh
+++ ChaosMesh/07_Deployment_Service.sh
@@ -1,2 +1,6 @@
 #It will take a moment to start. You can see the application status and the Pods status will soon report Running in the default namespace:
 kubectl apply -f web-show-deployment.yaml && kubectl apply -f web-show-service.yaml && kubectl get deployments,pods,services
+
+# we can scale it to more replicas
+kubectl apply -f podtato-head-web-app.yaml
+kubectl scale deployments/helloservice --replicas=2 -n demospace
